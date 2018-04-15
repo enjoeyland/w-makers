eafefa73670eaf6806babd1a5508f8fed97e79a4
enjoeyland <khmin1104@gmail.com>
2018-04-15 15:52:24 +0900

    add Coordinate System
    polar point, rectangular point

--- car/Car/CoordinateSystem.cpp
+++ car/Car/CoordinateSystem.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "CoordinateSystem.h"
+#include "utility.h"
+#include <math.h>
+using joey_utility::toRadian;
+using joey_utility::toDegree;
+using namespace coordinateSystem;
+
+
+// defining PolarPoint
+RectangularPoint PolarPoint::toRectangular() {
+	if (correspondingRP_Defined) {
+		return correspondingRP;
+	} else {
+		correspondingRP.x = radial_distance * cos(toRadian<T>(degree));
+		correspondingRP.y = radial_distance * sin(toRadian<T>(degree));
+		correspondingRP_Defined = true;
+		return correspondingRP;
+	}
+}
+
+void PolarPoint::move(T x, T y) {
+	toRectangular();
+	correspondingRP.move(x, y);
+	degree = correspondingRP.toPolar().degree;
+	radial_distance = correspondingRP.toPolar().radial_distance;
+}
+
+void PolarPoint::modifyDegree(T degree) {
+	this->degree += degree;
+}
+
+void PolarPoint::modifyRadialDistance(T radial_distance) {
+	this->radial_distance += radial_distance;
+}
+
+
+// defining RectangularPoint
+PolarPoint RectangularPoint::toPolar() {
+	if (correspondingRP_Defined) {
+		return correspondingPP;
+	} else {
+		correspondingPP.radial_distance = sqrt(2, pow(x, 2) + pow(y, 2));
+		correspondingPP.degree = toDegree(acos(correspondingPP.radial_distance));
+		correspondingRP_Defined = true;
+		return correspondingPP;
+	}
+}
+
+void RectangularPoint::move(T x, T y) {
+	this->x += x;
+	this->y += y;
+}
+
--- car/Car/CoordinateSystem.h
+++ car/Car/CoordinateSystem.h
@@ -5,4 +5,55 @@
 #ifndef CAR_COORDINATESYSTEM_H
 #define CAR_COORDINATESYSTEM_H
 
+namespace coordinateSystem {
+	class RectangularPoint;
+
+	template <typename T>
+	class PolarPoint
+	{
+	public:
+		T degree;
+		T radial_distance;
+
+	public:
+		PolarPoint(): degree(0), radial_distance(0), correspondingRP_Defined(false) {}
+		PolarPoint(T degree, T radial_distance): degree(degree), radial_distance(radial_distance), correspondingRP_Defined(
+				false) {}
+		~PolarPoint();
+
+	public:
+		RectangularPoint toRectangular();
+		void move(T x, T y);
+		void modifyDegree(T degree);
+		void modifyRadialDistance(T radial_distance);
+
+	private:
+		RectangularPoint correspondingRP;
+		bool correspondingRP_Defined;
+	};
+
+
+	template <typename T>
+	class RectangularPoint
+	{
+	public:
+		T x;
+		T y;
+
+	public:
+		RectangularPoint(): x(0), y(0), correspondingRP_Defined(false) {}
+		RectangularPoint(T x, T y): x(x), y(y), correspondingRP_Defined(false) {}
+		~RectangularPoint();
+
+	public:
+		PolarPoint toPolar();
+		void move(T x, T y);
+
+	private:
+		PolarPoint correspondingPP;
+		bool correspondingRP_Defined;
+	};
+}
+
+
 #endif //CAR_COORDINATESYSTEM_H
