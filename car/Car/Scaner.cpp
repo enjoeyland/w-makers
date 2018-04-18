bcf0f25f4958cefb1852cff405a91c44eec7d4aa
enjoeyland <khmin1104@gmail.com>
2018-04-18 16:11:20 +0900

    add Scaner class
    done with measureDistance, setServoDegree

--- car/Car/Scaner.cpp
+++ car/Car/Scaner.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "Scaner.h"
+#include <Arduino.h>
+#include <Servo.h>
+
+Scaner::Scaner(int pinUltrasonicTrig, int pinUltrasonicEcho, int pinServo, int pulseMin, int pulseMax):
+		pinUltrasonicTrig(pinUltrasonicTrig),
+		pinUltrasonicEcho(pinUltrasonicEcho),
+		pinServo(pinServo),
+		pulseMin(pulseMin),
+		pulseMax(pulseMax){
+	scanerServo.attach(this->pinServo, pulseMin, pulseMax);
+}
+
+double Scaner::measureDistance() { // measure distance with servo motor
+	digitalWrite(pinUltrasonicTrig, LOW); // init servo motor
+	delayMicroseconds(2);
+
+	digitalWrite(pinUltrasonicTrig, HIGH); // send pulse
+	delayMicroseconds(10);
+	digitalWrite(pinUltrasonicTrig, LOW);
+
+	double duration = pulseIn(pinUltrasonicEcho, HIGH); // wait pulse
+
+	double distance = duration * 0.0177; // 334m/s / 2(왕복 제거)
+	return distance;
+}
+
+PolarPoint<double>* Scaner::scan(double degree) {
+
+}
+
+PolarPoint<double>* Scaner::scanSpecific(double startDegree, double endDegree, double density) {
+
+}
+
+PolarPoint<double>* Scaner::scan180(double density) {
+
+}
+
+PolarPoint<double>* Scaner::scan360(double density) {
+
+}
+
+double Scaner::setServoDegree(double degree) {
+	long realPulseWidth = map(degree, 0, 180, pulseMin, pulseMax);
+	int practicalPulseWidth = static_cast<int>(realPulseWidth);
+	scanerServo.writeMicroseconds(practicalPulseWidth);
+	double diff = static_cast<double> (map(static_cast<long>(realPulseWidth) - practicalPulseWidth, pulseMin, pulseMax, 0, 180));
+	return diff; // return difference with param; unit as degree
+}
--- car/Car/Scaner.h
+++ car/Car/Scaner.h
@@ -5,4 +5,35 @@
 #ifndef CAR_SCANER_H
 #define CAR_SCANER_H
 
+#include "CoordinateSystem.h"
+using namespace coordinateSystem;
+
+class Scaner {
+private:
+	int pinUltrasonicTrig;
+	int pinUltrasonicEcho;
+	int pinServo;
+	int pulseMin;
+	int pulseMax;
+	Servo scanerServo;
+
+public:
+	Scaner(int pinUltrasonicTrig, int pinUltrasonicEcho, int pinServo, int pulseMin = 500, int pulseMax = 2400);
+	// default pulse min & max is based on TowerPro SG90 Servo
+	~Scaner();
+
+public:
+	double measureDistance();
+	PolarPoint<double>* scan(double degree); //return array of PolarPoint
+
+	// density is the number of scan per 1 degree
+	PolarPoint<double>* scanSpecific(double startDegree, double endDegree, double density = 1.0); 
+	PolarPoint<double>* scan180(double density = 1.0);
+	PolarPoint<double>* scan360(double density = 1.0);
+
+protected:
+	double setServoDegree(double degree); // return difference with param; unit as degree
+};
+
+
 #endif //CAR_SCANER_H
