815e9f3c98c170077c2c03c1c50b36822dfac011
enjoeyland <khmin1104@gmail.com>
2018-05-31 13:24:38 +0900

    change for debugging
    need change for debugging method some like main.cpp by mock of arduino

--- car/Car/car.ino
+++ car/Car/car.ino
@@ -1,31 +1,36 @@
-#include "CarMovement.h"
-//using namespace carMovement;
-#include "CoordinateSystem.h"
-using namespace coordinateSystem;
+// #include "CarMovement.h"
+// //using namespace carMovement;
+// #include "CoordinateSystem.h"
+// using namespace coordinateSystem;
 
-int pinLeftMotorGo = 9;     //좌측모터전진(IN2)
-int pinLeftMotorBack = 8;     //좌측모터후진(IN1)
+#include"CarPropertyFinder.h"
 
-int pinRightMotorGo = 10;    // 우측모터전진(IN3)
-int pinRightMotorBack = 11;    // 우측모터후진(IN4)
+// int pinLeftMotorGo = 9;     //좌측모터전진(IN2)
+// int pinLeftMotorBack = 8;     //좌측모터후진(IN1)
 
-float car_width = 20;
+// int pinRightMotorGo = 10;    // 우측모터전진(IN3)
+// int pinRightMotorBack = 11;    // 우측모터후진(IN4)
 
-CarMovement myCar = static_cast<CarMovement> (CarMovementAnalog(pinLeftMotorGo, pinLeftMotorBack, pinRightMotorGo, pinRightMotorBack, car_width));
+// float car_width = 20;
+
+// CarMovement myCar = static_cast<CarMovement> (CarMovementAnalog(pinLeftMotorGo, pinLeftMotorBack, pinRightMotorGo, pinRightMotorBack, car_width));
 
 
 void setup()
 {
+	Car c = Car();
+	CarPropertyFinder carPropertyFinder(c,(CarMovement) CarMovementAnalog(5,6,7,8,c), Scaner(9,10,11));
 }
 
 void loop()
 {
-	test_move();
+	// test_move();
+	delay(1000);
 }
 
-void test_move() {
-	myCar.goForward(255.0, 100.0);
-	myCar.goBackward(255.0, 100.0);
-	myCar.spinLeft(200.0, 0.0 , 360.0);
-	myCar.turnRight(200.0, 10.0, 360.0);
-}
+// void test_move() {
+// 	myCar.goForward(255.0, 100.0);
+// 	myCar.goBackward(255.0, 100.0);
+// 	myCar.spinLeft(200.0, 0.0 , 360.0);
+// 	myCar.turnRight(200.0, 10.0, 360.0);
+// }
